28b2ad0924aa6e702903f0f94c7b72c5ba0de412
krzysiek578 <krispil@o2.pl>
2022-11-03 17:26:36 +0100

    Finish testAddRentalOffice and refactore code

--- src/test/java/com/app/portfolio/database/DatabaseAndSpringBoot/databaseTest/CarSuite.java
+++ src/test/java/com/app/portfolio/database/DatabaseAndSpringBoot/databaseTest/CarSuite.java
@@ -24,7 +24,6 @@ public class CarSuite {
     @AfterEach
     public void reset() {
         carRepository.deleteAll();
-
     }
 
     @Test
--- src/test/java/com/app/portfolio/database/DatabaseAndSpringBoot/databaseTest/RentalOfficeSuite.java
+++ src/test/java/com/app/portfolio/database/DatabaseAndSpringBoot/databaseTest/RentalOfficeSuite.java
@@ -3,6 +3,7 @@ package com.app.portfolio.database.DatabaseAndSpringBoot.databaseTest;
 import com.app.portfolio.database.DatabaseAndSpringBoot.RentalOffice;
 import com.app.portfolio.database.DatabaseAndSpringBoot.RentalOfficeRepository;
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,7 +23,9 @@ public class RentalOfficeSuite {
         //Given
         RentalOffice rentalOffice = new RentalOffice("Office in Warsaw", "Warsaw", "Ul.Trakt Brzeski", "12-345");
         //When
-
+        rentalOfficeRepository.save(rentalOffice);
         //Then
+        Assertions.assertTrue(rentalOfficeRepository.existsById(rentalOffice.getId()));
+        Assertions.assertEquals(rentalOffice, rentalOfficeRepository.findById(rentalOffice.getId()));
     }
 }
